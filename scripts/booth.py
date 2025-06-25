099c3af10bf26a6eb69a6be02400395ce773705b
Richard Teel <richteel@teelsys.com>
2025-06-25 15:18:13 -0400

    Reorganizing file structure

--- scripts/booth.py
+++
@@ -1,9 +0,0 @@
-"""
-Main script for the photobooth booth application.
-"""
-
-from booth_controller import BoothController
-
-if __name__ == "__main__":
-    photobooth = BoothController()
-    photobooth.main()
--- scripts/booth_model.py
+++
@@ -1,8 +0,0 @@
-"""Model Class for Booth Management System."""
-
-
-class BoothModel:
-    """Model class for managing the booth application."""
-
-    def __init__(self):
-        """Initialize the BoothModel."""
